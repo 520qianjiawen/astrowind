507332df1427a1e3112a907a1179c6a0bc3e4c12
caovn <520qianjiawen@sina.com>
2024-09-11 19:29:43 +0800

    recycling

--- src/navigation.js
+++ src/navigation.js
@@ -4,7 +4,7 @@ export const headerData = {
   links: [
     {
       text: 'Home',
-      href: getPermalink('/homes'),
+      href: getPermalink('/home'),
     },
     {
       text: 'Complete Solution',
